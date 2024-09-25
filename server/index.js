32ff5625b0282b3bdb4e4182b9e41806f11688c0
uruzsiddiqui <133843145+uruzsiddiqui@users.noreply.github.com>
2024-09-25 19:17:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,16 +14,14 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// app.use('/',(req, res) => {
-//     res.send("This is a stack overflow clone API")
-// })
+app.get('/',(req, res) => {
+    res.send("A2 is Running");
+});
 
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
 app.use("/answer", answerRoutes);
-app.get("/", (req,res) => {
-  res.send("A2 is running perfectly");
-})
+
 
 const PORT = process.env.PORT || 5000;
 
